67d05161f4644eeb17fcc55f2dc67329560ef7e1
hieurtrung <hieurteu@gmail.com>
2017-12-14 21:12:56 +0700

    Da co gang het suc

---
+++ PPC_Rental.AcceptanceTests/EditofagencyprojectSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace PPC_Rental.AcceptanceTests
+{
+    [Binding]
+    public class EditofagencyprojectSteps
+    {
+        [Given(@"Mở trang trủ(.*)")]
+        public void GivenMởTrangTrủ(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
